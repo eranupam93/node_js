a708947e2960a74d162c612b46258651b0c5d994
eranupam93 <eranupam93@gmail.com>
2025-03-26 14:53:31 +0530

    es module and common js module

---
+++ index.js
@@ -0,0 +1,5 @@
+// const { sum, add } = require('./util') common js module
+import { sum } from './util.js'
+
+
+console.log(sum(12, 3))
---
+++ util.js
@@ -0,0 +1,5 @@
+// const sum = (a, b) => a * b
+// const add = (a, b) => a + b
+
+// module.exports = { sum, add } 
+export const sum = (a, b) => a * b
